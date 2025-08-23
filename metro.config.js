c1153e5cbfc7e48265063e78fabbd6515df5b9fe
Oluwatayo Salami <104727347+cephasalami@users.noreply.github.com>
2025-08-23 01:44:03 +0100

    Fix Metro configuration error

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Add the React Native Worklets plugin to resolve the warning
-config.transformer.babelTransformerPath = require.resolve('react-native-worklets/plugin');
-
 module.exports = config;
